3b7efbffa6a19d3c0c13434e9325d2a7a73c052c
Frederico Falcao <fredericomfalcao@hotmail.com>
2022-11-17 16:30:31 +0200

    fixed bug

--- sys/db.php
+++ sys/db.php
@@ -1,11 +1,36 @@
 <?php
-require_once __DIR__."/../_implementation.php";
-	
-$db = new PDO('mysql:dbname='.$DB_NAME.';host='.$DB_HOST, $DB_USER_NAME, $DB_PASSWORD);
 
+/*
+*
+*	LIBRARY
+*
+*/
 function select($cols, $tblName, $extra = "") {
 	global $db;
 	$stmt = $db->prepare("SELECT ".implode(", ", $cols)." FROM $tblName $extra");
 	$stmt->execute();
 	return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
+
+
+
+/*
+*
+*	EXECUTABLE CODE
+*
+*/
+
+// 1. Fetch credentials
+require_once __DIR__."/../_implementation.php";	
+// 2. Connect to Database
+$db = new PDO('mysql:dbname='.$DB_NAME.';host='.$DB_HOST, $DB_USER_NAME, $DB_PASSWORD);
+
+// 3. Get CURRENT USER
+$currentUser = "xxx";
+
+
+
+
+// 3. HANDLE FILE UPLOADS (memes)
+if(isset($_FILES["meme"]) && isset($_FILES["meme"]["tmp_name"])) 
+	move_uploaded_file($_FILES["meme"]["tmp_name"], __DIR__."/uploads/$currentUser.jpg");
