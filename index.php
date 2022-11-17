779d14e5bac8e313990c8496617b801b809c1003
Frederico Falcao <fredericomfalcao@hotmail.com>
2022-11-17 16:21:32 +0200

    fixed bug

--- index.php
+++ index.php
@@ -8,12 +8,16 @@
 	<h1>ENVMA 2022</h1>
 	<?php foreach(select(["NomeLongo","FraseEpica"], "Utilizadores","LIMIT 1") as $row): extract($row); ?>
 		<h3><?=$FraseEpica?></h3>
-		<h5><?=$NomeLong?></h5>
+		<h5><?=$NomeLongo?></h5>
 	<?php endforeach; ?>
 	
 	<hr/>
 	
 	<h2>Memes</h2>
+
+	<form action="/" method="POST" enctype="multipart/form-data">
+		<input type="file" name="meme" id="meme">
+	</form>
 
 	<?php foreach(select(["NomeCurto"], "Ranking","LIMIT 3") as $row): extract($row); ?>
 		<img src="/uploads/<?=$NomeCurto?>.jpg" width="200" />
