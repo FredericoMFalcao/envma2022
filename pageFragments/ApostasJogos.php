2bcaaf7814d48782dbefc4bac3150835a94448d7
Frederico Falcao <fredericomfalcao@hotmail.com>
2022-11-19 17:57:43 +0200

    fixed bug / improved code

--- pageFragments/ApostasJogos.php
+++ pageFragments/ApostasJogos.php
@@ -17,7 +17,7 @@
 			"b.GolosEqFora ResultadoEqFora",
 			"b.EquipaCasa NomeEquipaCasa",
 			"b.EquipaFora NomeEquipaFora"
-		],"ApostasJogos a", "INNER JOIN Jogos b ON a.JogoId = b.JogoId AND a.JogoId = {$Jogos["JogoId"]} ORDER BY JogoId ASC") as $row) : extract($row); ?>
+		],"ApostasJogos a", "INNER JOIN Jogos b ON a.JogoId = b.JogoId AND a.JogoId = {$Jogos["JogoId"]} ORDER BY a.JogoId ASC") as $row) : extract($row); ?>
 			<?php if ($Utilizador != $currentUser && $Estado == "ApostasAbertas") continue; ?>
 			<tr>
 				<td>
