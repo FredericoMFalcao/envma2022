f0b8f4d7814d11b5ec3bf2bc91d75e28aac23536
Frederico Falcao <fredericomfalcao@hotmail.com>
2022-11-19 14:32:04 +0200

    fixed bug / improved code

--- pageFragments/ApostasJogos.php
+++ pageFragments/ApostasJogos.php
@@ -35,7 +35,7 @@
 						<select name="GolosEqFora">
 							<option>0</option><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option><option>6</option><option>7</option><option>8</option><option>9</option>
 						</select>
-						Boost: <input type="checkbox" name="boost" <?=$Boost?"checked":""?> />
+						<br/>Boost: <input type="checkbox" name="boost" <?=$Boost?"checked":""?> />
 						<br/><input type="submit" />
 					</form>
 				<?php else : ?>
