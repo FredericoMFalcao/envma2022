47a07edaedaf4e29314f08ce445b5e4d110795f9
Frederico Falcao <fredericomfalcao@hotmail.com>
2022-11-20 20:22:06 +0200

    fixed bug / improved code

--- pageFragments/Ranking.php
+++ pageFragments/Ranking.php
@@ -6,7 +6,7 @@
 		</tr>
 	</thead>
 <tbody>
-	<?php foreach(select(["Utilizador","Pontos"],"ApostasJogosComPontosCalculados") as $row) : extract($row); ?>
+	<?php foreach(select(["Utilizador","Pontos"],"Ranking") as $row) : extract($row); ?>
 		<tr>
 			<td><?=$Utilizador?></td>
 			<td><?=$Pontos?></td>
