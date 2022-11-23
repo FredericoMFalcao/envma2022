13b2aa616dd38627d0dd87334960dd425e879548
Frederico Falcao <fredericomfalcao@hotmail.com>
2022-11-23 14:37:58 +0200

    fixed bug / improved code

--- pageFragments/DadosPessoais.php
+++ pageFragments/DadosPessoais.php
@@ -1,8 +1,9 @@
 <form action="" method="POST">
 <?php foreach(select(["NomeLongo","FraseEpica"], "Utilizadores","WHERE Utilizador = '$currentUser' LIMIT 1") as $row): extract($row); ?>
 	Frase Épica:
-	<input type="hidden" name="_table" value="Utilizadores">
-	<input type="text" name="FraseEpica" value="<?=$FraseEpica?>">
+	<input type="hidden" name="_table"     value="Utilizadores">
+	<input type="text"   name="NomeLongo"  value="<?=$NomeLongo?>">
+	<input type="text"   name="FraseEpica" value="<?=$FraseEpica?>">
 	<input type="submit">
 <?php endforeach; ?>
 </form>
