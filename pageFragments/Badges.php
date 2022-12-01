a9be33e7224ca7531028e218a28c3a797fb4f526
Frederico Falcao <fredericomfalcao@hotmail.com>
2022-12-01 15:48:17 +0200

    fixed bug / improved code

--- pageFragments/Badges.php
+++ pageFragments/Badges.php
@@ -6,19 +6,19 @@
 		</tr>
 	</thead>
 <tbody>
-	<?php foreach(select(["Utilizador","MaiorSeqDeCincos"],"EstatisticasUtilizadores", "ORDER BY MaiorSeqDeCincos DESC LIMIT 1") as $row) : extract($row); ?>
+	<?php foreach(select(["b.NomeLongo Utilizador","MaiorSeqDeCincos"],"EstatisticasUtilizadores a", "INNER JOIN Utilizadores b ON a.Utilizador = b.Utilizador ORDER BY MaiorSeqDeCincos DESC LIMIT 1") as $row) : extract($row); ?>
 		<tr>
 			<td>Maior Sequência de 5's</td>
 			<td><?=$Utilizador?></td>
 		</tr>
 	<?php endforeach;?>
-	<?php foreach(select(["Utilizador","MaiorSeqDeTendenciaCorreta"],"EstatisticasUtilizadores", "ORDER BY MaiorSeqDeTendenciaCorreta DESC LIMIT 1") as $row) : extract($row); ?>
+	<?php foreach(select(["b.NomeLongo Utilizador","MaiorSeqDeTendenciaCorreta"],"EstatisticasUtilizadores a", "INNER JOIN Utilizadores b ON a.Utilizador = b.Utilizador ORDER BY MaiorSeqDeTendenciaCorreta DESC LIMIT 1") as $row) : extract($row); ?>
 		<tr>
 			<td>Maior Sequência de Tendência Correta</td>
 			<td><?=$Utilizador?></td>
 		</tr>
 	<?php endforeach;?>
-	<?php foreach(select(["Utilizador","MaiorContribuinteDeResultados"],"EstatisticasUtilizadores", "ORDER BY MaiorContribuinteDeResultados DESC LIMIT 1") as $row) : extract($row); ?>
+	<?php foreach(select(["b.NomeLongo Utilizador","a.MaiorContribuinteDeResultados"],"EstatisticasUtilizadores a", "INNER JOIN Utilizadores b ON a.Utilizador = b.Utilizador ORDER BY a.MaiorContribuinteDeResultados DESC LIMIT 1") as $row) : extract($row); ?>
 		<tr>
 			<td>Maior Contribuinte de Resultados</td>
 			<td><?=$Utilizador?></td>
