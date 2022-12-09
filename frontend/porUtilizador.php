9d76b4d1ab4a5426556e226d16c4c893633bc16e
Frederico Falcao <fredericomfalcao@hotmail.com>
2022-12-09 16:05:18 +0200

    fixed bug / improved code

--- frontend/porUtilizador.php
+++ frontend/porUtilizador.php
@@ -7,6 +7,7 @@
 			<tr>
 				<th>Jogo</th>
 				<th>Fase</th>
+				<th>Resultado</th>
 				<th>Aposta</th>
 				<th>Pontos</th>
 				<th>Data Aposta</th>
@@ -14,8 +15,9 @@
 		<tbody>
 			<?php foreach(select(["EqCasa","EqFora","Fase","ResultadoEqCasa","ResultadoEqFora","Pontos","DataHoraAposta","Boost","ApostaGolosEqCasa","ApostaGolosEqFora"],"vApostasJogos","WHERE UtilizadorSigla = '{$_GET['UtilizadorSigla']}'") as $row) :  extract($row);?>
 				<tr>
-					<td><?=$EqCasa?> - <?=$EqFora?> (<?=$ResultadoEqCasa?> - <?=$ResultadoEqFora?>)</td>
+					<td><?=$EqCasa?> - <?=$EqFora?> </td>
 					<td><?=$Fase?></td>
+					<td><?=$ResultadoEqCasa?> - <?=$ResultadoEqFora?></td>
 					<td><?=$ApostaGolosEqCasa?> - <?=$ApostaGolosEqFora?> <?=($Boost == 1?"(b)":"")?></td>
 					<td><?=$Pontos?></td>
 					<td><?=$DataHoraAposta?></td>
